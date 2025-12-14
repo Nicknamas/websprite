9a8cb9d3e1756414bcd2d493d669fb2252224e27
Woadex <a.kakunin23@gmail.com>
2025-12-14 12:13:00 +0800

    update App.vue

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import HelloWorld from "./components/HelloWorld.vue";
 
 <template>
   <div>
-    hello from someone
+    hello from me
     <a href="https://vite.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
